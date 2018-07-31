46b54a232e7eb4848b38912ea52a86ca63c2c199
tammypmc <tammymoscoso2@gmail.com>
2018-07-31 13:51:02 -0600

    arreglar url

--- docs/js/GeojsonMapService.js
+++ docs/js/GeojsonMapService.js
@@ -1,7 +1,7 @@
 function GeojsonMapService(){
 
     //this.map = map;
-    this.urlData = 'https://raw.githubusercontent.com/tammypmc/lascallesdelasmujeres/master';
+    this.urlData = 'https://raw.githubusercontent.com/tammypmc/lascallesdelasmujeres/callescostarica';
 
     this.loadGeojson = function(map, folder, isMobile, coords){    
     
